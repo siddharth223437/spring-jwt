41b6bc6c60202c36c3490c2c8cc683a06ea7d05f
siddharth2223437 <siddharthsarathe.nmims@gmail.com>
2020-02-23 11:34:43 -0600

    Initial commit

---
+++ src/test/java/com/sid/spring/jwt/springjwt/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.spring.jwt.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
